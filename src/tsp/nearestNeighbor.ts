01bb43e411d4edf66fefe00141b9823ac76145f4
nosequeponeraquiXD <140538914+nosequeponeraquiXD@users.noreply.github.com>
2026-03-28 13:45:55 -0300

    atualização  nearestNeighbor.ts

--- src/tsp/nearestNeighbor.ts
+++ src/tsp/nearestNeighbor.ts
@@ -4,6 +4,18 @@ export function nearestNeighbor(matrix: number[][]): number {
 
   let current = 0;
   visited[current] = true;
+  et cost = 0;
+
+  for (let i = 0; i < n - 1; i++) {
+    let next = -1;
+    let min = Infinity;
+
+    for (let j = 0; j < n; j++) {
+      if (!visited[j] && matrix[current][j] < min) {
+        min = matrix[current][j];
+        next = j;
+      }
+    }
 
  
 }
